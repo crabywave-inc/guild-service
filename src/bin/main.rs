54889af2d713f0d1c4c8f27bb57f811c52134d5e
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-24 00:54:14 +0100

    feat: create firestore database and create repository & service to guilds

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,12 @@
 use clap::Parser;
 
-use std::sync::Arc;
 use guild::application::http::{HttpServer, HttpServerConfig};
 use guild::application::ports::messaging_ports::{MessagingType, MessagingTypeImpl};
+use guild::domain::guild::services::GuildServiceImpl;
 use guild::env::Env;
-
+use guild::infrastructure::db::firestore::Firestore;
+use guild::infrastructure::guild::db::firestore_guild_repository::FirestoreGuildRepository;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -13,10 +15,15 @@ async fn main() -> anyhow::Result<()> {
 
     let env = Arc::new(Env::parse());
 
-    let _messaging_port = Arc::new(MessagingTypeImpl::new(&MessagingType::PubSub, Arc::clone(&env)).await?);
+    let _messaging_port =
+        Arc::new(MessagingTypeImpl::new(&MessagingType::PubSub, Arc::clone(&env)).await?);
+
+    let firestore = Arc::new(Firestore::new(Arc::clone(&env)).await?);
+    let guild_repository = FirestoreGuildRepository::new(Arc::clone(&firestore));
+    let guild_service = Arc::new(GuildServiceImpl::new(guild_repository));
 
     let server_config = HttpServerConfig::new(env.port.clone());
-    let http_server = HttpServer::new(server_config).await?;
+    let http_server = HttpServer::new(server_config, Arc::clone(&guild_service)).await?;
 
     http_server.run().await
 }
